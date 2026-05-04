7c7034aabb052a71de8ad0ed4f544084b4553bbd
daniel.stein <daniel.l.stein6.civ@us.navy.mil>
2026-05-04 09:08:03 -0400

    Cycled through states

--- hexgen.js
+++ hexgen.js
@@ -4,13 +4,14 @@ const nCols = 20;
 document.addEventListener("DOMContentLoaded", async function() {
     const hexGridDiv = document.getElementById("grid-container");
 
+    hexGridDiv.addEventListener('click', (e) => hexClickEvent(e));
 
     const hexCol = document.createElement("div");
     hexCol.classList.add("hex-col");
 
     const hex = document.createElement("div");
     hex.classList.add("hex-border");
-    hex.innerHTML = '<div class="hex-cell">\n<div class="hex-cover"></div>\n</div>';
+    hex.innerHTML = '<div class="hex-cell c0">\n<div class="hex-cover"></div>\n</div>';
 
     for (let rr = 0; rr < nRows; rr++){
         hexCol.appendChild(hex.cloneNode(true));
@@ -26,3 +27,20 @@ document.addEventListener("DOMContentLoaded", async function() {
     }
 
 });
+
+function hexClickEvent(event){
+    const target = event.target;
+
+    const hex = target.closest('.hex-cell');
+    const currentClass = [...hex.classList].find(cls => /^c[0-6]$/.test(cls));
+
+    if (currentClass) {
+        const currentIndex = parseInt(currentClass.substring(1));
+        const nextIndex = (currentIndex + 1) % 7;
+
+        hex.classList.remove(currentClass);
+        hex.classList.add(`c${nextIndex}`);
+    } else {
+        hex.classList.add("c0");
+    }
+}
