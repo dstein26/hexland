283d9faa4d34acbbc93e4b5b324ec7b66ccb48be
daniel.stein <daniel.l.stein6.civ@us.navy.mil>
2026-05-04 08:54:48 -0400

    Generate hexgrid script

---
+++ hexgen.js
@@ -0,0 +1,28 @@
+const nRows = 7;
+const nCols = 20;
+
+document.addEventListener("DOMContentLoaded", async function() {
+    const hexGridDiv = document.getElementById("grid-container");
+
+
+    const hexCol = document.createElement("div");
+    hexCol.classList.add("hex-col");
+
+    const hex = document.createElement("div");
+    hex.classList.add("hex-border");
+    hex.innerHTML = '<div class="hex-cell">\n<div class="hex-cover"></div>\n</div>';
+
+    for (let rr = 0; rr < nRows; rr++){
+        hexCol.appendChild(hex.cloneNode(true));
+    }
+
+    for (let cc=0; cc < nCols; cc++){
+        const colClone = hexCol.cloneNode(true)
+        if (Math.pow(-1, cc) < 0) {
+            colClone.classList.add("offset");
+        }
+
+        hexGridDiv.append(colClone);
+    }
+
+});
