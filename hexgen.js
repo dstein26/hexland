3effcf2dd43910fa426fefb3529f0ddff8eb850c
daniel.stein <daniel.l.stein6.civ@us.navy.mil>
2026-05-04 09:36:28 -0400

    Can select neighbors

--- hexgen.js
+++ hexgen.js
@@ -19,9 +19,16 @@ document.addEventListener("DOMContentLoaded", async function() {
 
     for (let cc=0; cc < nCols; cc++){
         const colClone = hexCol.cloneNode(true)
-        if (Math.pow(-1, cc) < 0) {
+        if (cc%2 !== 0) {
             colClone.classList.add("offset");
         }
+
+        const hexesInCol = colClone.querySelectorAll('.hex-cell');
+
+        hexesInCol.forEach((hex, rr) => {
+            hex.dataset.row = rr;
+            hex.dataset.col = cc;
+        });
 
         hexGridDiv.append(colClone);
     }
@@ -32,6 +39,25 @@ function hexClickEvent(event){
     const target = event.target;
 
     const hex = target.closest('.hex-cell');
+    // getNeighborHexes(hex).forEach((h) => cycleState(h));
+    cycleState(hex);
+}
+
+function setState(hex, state){
+    const currentClass = [...hex.classList].find(cls => /^c[0-6]$/.test(cls));
+
+    if (currentClass) {
+        const currentIndex = parseInt(currentClass.substring(1));
+        const nextIndex = (state) % 7;
+
+        hex.classList.remove(currentClass);
+        hex.classList.add(`c${nextIndex}`);
+    } else {
+        hex.classList.add("c0");
+    }
+}
+
+function cycleState(hex){
     const currentClass = [...hex.classList].find(cls => /^c[0-6]$/.test(cls));
 
     if (currentClass) {
@@ -43,4 +69,19 @@ function hexClickEvent(event){
     } else {
         hex.classList.add("c0");
     }
+}
+
+function getNeighborHexes(cHex) {
+    const col = parseInt(cHex.dataset.col);
+    const row = parseInt(cHex.dataset.row);
+
+    const isOdd = col % 2 !== 0;
+
+    const dir = isOdd ? 
+        [[0, -1], [0, 1], [-1, 0], [-1, 1], [1, 0], [1, 1]] :
+        [[0, -1], [0, 1], [-1, -1], [-1, 0], [1, -1], [1, 0]];
+
+    return dir.map(([dc, dr]) => {
+        return document.querySelector(`.hex-cell[data-col="${col+dc}"][data-row="${row+dr}"]`);
+    }).filter(n => n !== null);
 }
